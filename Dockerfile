036f6e9831a5c3bffde9bcd93a923e9546f7b424
IgnOrtega <118409806+IgnOrtega@users.noreply.github.com>
2025-09-29 23:33:17 -0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Imagen base
+FROM python:3.12
+
+# Directorio de trabajo
+WORKDIR /app
+
+# Crear el directorio 'results' en el contenedor
+RUN mkdir -p /app/serializar_modelo
+
+# Copiar archivos, incluyendo el modelo desde la carpeta 'results'
+COPY serializar_modelo/Modelo_Breast_Cancer.pkl /app/serializar_modelo/
+COPY app.py .
+COPY requirements.txt .
+# Instalar dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Exponer puerto
+EXPOSE 5000
+
+# Ejecutar app
+CMD ["python", "app.py"]
+
+#- Para crear el contenedor
+# docker build -t breast-cancer-api .
---
+++ app.py
@@ -0,0 +1,84 @@
+# app.py
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+dir="./serializar_modelo"
+# ==========================================
+# 1. Inicializar Flask
+# ==========================================
+app = Flask(__name__)
+
+# ==========================================
+# 2. Cargar modelo serializado
+# ==========================================
+modelo = joblib.load(dir+"/"+"Modelo_Breast_Cancer.pkl")
+
+# El dataset Iris tiene 30 features + 1 target
+N_FEATURES = 30
+
+# ==========================================
+# 3. Ruta principal
+# ==========================================
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"mensaje": "✅ API lista"})
+
+# ==========================================
+# 4. Endpoint de predicción
+# ==========================================
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Leer JSON
+        data = request.get_json()
+
+        # Validar que tenga la clave "features"
+        if "features" not in data:
+            return jsonify({"error": "Falta la clave 'features' en el JSON"}), 400
+
+        features = data["features"]
+
+        # Validar que sea lista numérica
+        if not isinstance(features, list):
+            return jsonify({"error": "'features' debe ser una lista"}), 400
+
+        if len(features) != N_FEATURES:
+            return jsonify({
+                "error": f"Se esperaban {N_FEATURES} valores, pero se recibieron {len(features)}"
+            }), 400
+
+        try:
+            features = [float(x) for x in features]
+        except ValueError:
+            return jsonify({"error": "Todos los valores en 'features' deben ser numéricos"}), 400
+
+        # Convertir a array numpy
+        features_array = np.array(features).reshape(1, -1)
+
+        # Realizar predicción
+        prediction = modelo.predict(features_array)
+
+        return jsonify({
+            "prediction": int(prediction[0])
+        })
+
+    except Exception as e:
+        return jsonify({"error": f"Error interno: {str(e)}"}), 500
+
+
+# ==========================================
+# 5. Ejecutar servidor
+# ==========================================
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+"""
+Ejecutar estos código en la powershell:
+
+curl -Method POST http://127.0.0.1:5000/predict `
+  -Headers @{ "Content-Type" = "application/json" } `
+  -Body '{"features": [1.93490586,  0.99373946,  1.93309591,  2.01678415,  0.30883801,1.06619158,  2.29003532,  2.1171921 ,  1.43621312, -0.54118592,2.16129581, -0.71857712,  1.735548  ,  2.14655106, -0.58601673,0.76020176,  2.09838476,  1.11014072,  0.41986408,  0.45658573,1.92810613,  0.21540599,  1.72873394,  1.98541627, -0.49396886,0.40035425,  2.04811805,  1.46013593,  0.36439584, -0.30233857]}'
+    
+"""
